06cc82ff0ee0d85fbd651eef987fab463b6ddcb9
pranavarora1895 <aurorapranav187@gmail.com>
2020-09-24 20:18:15 +0530

    Added to_ascii.py: variable character renamed

--- to_ascii.py
+++ to_ascii.py
@@ -1,13 +1,13 @@
-def to_ascii(c):
+def to_ascii(character):
     lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
              'u', 'v', 'w', 'x', 'y', 'z']
     ascii_code_lower = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118,
                   119, 120, 121, 122]
     upper = ['A', 'B', 'C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     ascii_code_upper = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90]
-    if c in lower:
-        index = lower.index(c)
+    if character in lower:
+        index = lower.index(character)
         print(ascii_code_lower[index])
-    elif c in upper:
-        index = upper.index(c)
+    elif character in upper:
+        index = upper.index(character)
         print(ascii_code_upper[index])
