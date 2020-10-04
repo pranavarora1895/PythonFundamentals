3bade02634ebba221bf194030f2064991b7a4746
pranavarora1895 <aurorapranav187@gmail.com>
2020-10-04 21:14:54 +0530

    new programs added

--- string_practise.py
+++ string_practise.py
@@ -25,3 +25,33 @@ class StringPractise:
         Str_list.pop(char_idx - 1)
 
         print(new_String.join(Str_list))
+
+    def is_substring(self, substring):
+        print(substring in self.inp_string)
+
+    def even_char_words(self):
+        input_string = self.inp_string
+        even_list = []
+        even_string = ''
+        ls_list = input_string.split(' ')
+        for character in ls_list:
+            if len(character) % 2 == 0 and not character.isdigit():
+                even_list.append(character + ', ')
+
+        join_str = even_string.join(even_list)
+        print(f'The words with even number of digits are {join_str}')
+
+    def acceptable_if_vowels(self):
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        present_vowels = []
+        inp_vow_list = self.inp_string
+        for i in inp_vow_list.lower():
+            for j in vowels:
+                if j in i:
+                    present_vowels.append(j)
+
+        if ('a' and 'e' and 'i' and 'o' and 'u') in present_vowels:
+
+            print("Acceptable")
+        else:
+            print('Not Acceptable')
